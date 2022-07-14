62f2fc2ddc94292d5873bffebbc6cf5f6659c475
Vika <vpredko@mail.ru>
2022-07-14 20:33:05 +0300

    Solve task1

---
+++ task1/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] CreateRandomArray(int min, int max)
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    Console.Write ("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine("] ");
+
+}
+
+Console.WriteLine ("Введите певрое число");
+int start = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("Введите последнее число");
+int end = Convert.ToInt32 (Console.ReadLine());
+
+int[] myArray = CreateRandomArray(start, end);
+ShowArray(myArray);
+
+int count = 0;
+
+for (int i = 0; i < myArray.Length; i++)
+
+   if (myArray[i] > 0)
+        count = count + 1;
+
+Console.WriteLine ("Чисел больше 0 -> "+ count);
