f54ea8ec1acaedd24d607b03c16bef8389b1b4ea
Vika <vpredko@mail.ru>
2022-07-15 18:59:41 +0300

    Solve task2

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+if (k1 == k2)
+    if (b1 == b2)
+        Console.WriteLine("Точек пересечения бесконечно много");
+    else
+        Console.WriteLine("Точек пересечения нет, прямые параллельны");
+else
+    Console.WriteLine("Координаты точек пересечения: ");
+    double x = (b1-b2)/(k2-k1);
+    double y = k1 * x + b1;
+    Console.WriteLine(x + " , " + y);
+    
+        
+    
